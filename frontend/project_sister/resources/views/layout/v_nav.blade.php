0a442bdb642d395cd01847e81d1ea1b8da7f9a35
Raka Surya Kusuma <58557156+RakaSK@users.noreply.github.com>
2021-06-28 19:15:34 +0700

    add tables n forms

--- frontend/project_sister/resources/views/layout/v_nav.blade.php
+++ frontend/project_sister/resources/views/layout/v_nav.blade.php
@@ -1,20 +1,28 @@
 <ul class="sidebar-menu" data-widget="tree">
     <li class="header">MAIN NAVIGATION</li>
     <li><a href="/"><i class="fa fa-home"></i> <span>Home</span></a></li>
-    <li><a href="/TabelStok"><i class="fa fa-book"></i> <span>Tabel Stock</span></a></li>
-    <li><a href="/FormStok"><i class="fa fa-book"></i> <span>Form Stock</span></a></li>
-    <li><a href="/TabelProduk"><i class="fa fa-book"></i> <span>Tabel Produk</span></a></li>
-    <li><a href="/FormProduk"><i class="fa fa-book"></i> <span>Form Produk</span></a></li>
     <li class="treeview">
-        <a href="#">
-            <i class="fa fa-share"></i> <span>Multilevel</span>
+          <a href="#">
+            <i class="fa fa-table"></i> <span>Tables</span>
             <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
+              <i class="fa fa-angle-left pull-right"></i>
             </span>
-        </a>
-        <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-        </ul>
-    </li>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="/TabelStok"><i class="fa fa-circle-o"></i> Tabel Stock</a></li>
+            <li><a href="/TabelProduk"><i class="fa fa-circle-o"></i> Tabel Produk</a></li>
+          </ul>
+        </li>
+    <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Forms</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="/FormStok"><i class="fa fa-circle-o"></i>Form Stock</a></li>
+            <li><a href="/FormProduk"><i class="fa fa-circle-o"></i> Form Produk</a></li>
+          </ul>
+        </li>
 </ul>
