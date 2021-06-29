85e66f637aac6d8283b1337b213888d46328ff9c
Yopi Junita Ambarita <yopiambarita06@student.uns.ac.id>
2021-06-29 09:01:12 +0700

    update template

--- frontend/project_sister/resources/views/layout/v_nav.blade.php
+++ frontend/project_sister/resources/views/layout/v_nav.blade.php
@@ -1,28 +1,29 @@
 <ul class="sidebar-menu" data-widget="tree">
-    <li class="header">MAIN NAVIGATION</li>
-    <li><a href="/"><i class="fa fa-home"></i> <span>HOME</span></a></li>
-    <li class="treeview">
-          <a href="#">
-            <i class="fa fa-table"></i> <span>TABLES</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="/TabelStok"><i class="fa fa-circle-o"></i> Tabel Stock</a></li>
-            <li><a href="/TabelProduk"><i class="fa fa-circle-o"></i> Tabel Produk</a></li>
-          </ul>
-        </li>
-    <li class="treeview">
-          <a href="#">
-            <i class="fa fa-edit"></i> <span>FORMS</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="/FormStok"><i class="fa fa-circle-o"></i>Form Stock</a></li>
-            <li><a href="/FormProduk"><i class="fa fa-circle-o"></i> Form Produk</a></li>
-          </ul>
-        </li>
+  <li class="header">MAIN NAVIGATION</li>
+  <li><a href="/"><i class="fa fa-home"></i> <span>HOME</span></a></li>
+  <li class="treeview">
+    <a href="#">
+      <i class="fa fa-table"></i> <span>TABLES</span>
+      <span class="pull-right-container">
+        <i class="fa fa-angle-left pull-right"></i>
+      </span>
+    </a>
+    <ul class="treeview-menu">
+      <li><a href="/TabelStok"><i class="fa fa-circle-o"></i> Tabel Stock</a></li>
+      <li><a href="/TabelProduk"><i class="fa fa-circle-o"></i> Tabel Produk</a></li>
+    </ul>
+  </li>
+  <li class="treeview">
+    <a href="#">
+      <i class="fa fa-edit"></i> <span>FORMS</span>
+      <span class="pull-right-container">
+        <i class="fa fa-angle-left pull-right"></i>
+      </span>
+    </a>
+    <ul class="treeview-menu">
+      <li><a href="/FormStok"><i class="fa fa-circle-o"></i>Form Stock</a></li>
+      <li><a href="/FormProduk"><i class="fa fa-circle-o"></i> Form Produk</a></li>
+    </ul>
+  </li>
+  <li><a href="/"><i class="fa fa-power-off"></i> <span>LOGOUT</span></a></li>
 </ul>
